82982fae5f15dff677d0ef722dfb5876bcec8f0c
Andrea Cognolato <andrecogno@hotmail.it>
2021-12-28 17:28:16 +0100

    Initial tailwind CSS homepage

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "index.html",
+    "index-style.css"
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': "'Source Sans Pro', sans-serif",
+      'mono': "ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
+    }
+  },
+  plugins: [],
+}
