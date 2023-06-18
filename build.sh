0f77ae04be60e8c8d4eea20c8a533e6d99afb82a
Andrea Cognolato <andrecogno@hotmail.it>
2023-06-18 16:04:41 +0200

    Move stuff into src

--- build.sh
+++ build.sh
@@ -7,11 +7,14 @@ set -o pipefail
 if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 
 BUILD_DIR="_site"
+SOURCE_DIR="src"
 
 rm -rf "$BUILD_DIR" && mkdir "$BUILD_DIR"
 
-cp index.html "$BUILD_DIR"
+cp "$SOURCE_DIR"/index.html "$BUILD_DIR"
+cp "$SOURCE_DIR"/404.html "$BUILD_DIR"
+cp "$SOURCE_DIR"/hes_my_quant.pdf "$BUILD_DIR"
 
-cp -r blog "$BUILD_DIR"
+cp -r "$SOURCE_DIR"/blog "$BUILD_DIR"
 
-cp -r assets "$BUILD_DIR"
+cp -r "$SOURCE_DIR"/assets "$BUILD_DIR"
